d2cb0a371658763ec25df0ea6c96fbfef1bf65e5
CodeNinjaDan <dnlmuiru@gmail.com>
2024-12-18 22:46:14 +0300

    added flight data

--- flight-deals/main.py
+++ flight-deals/main.py
@@ -1,6 +1,7 @@
 import time
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
 
 # ==================== Set up the Flight Search ====================
 
@@ -8,6 +9,8 @@ data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 # print(sheet_data)
 flight_search = FlightSearch()
+price_data = sheet_data['lowestPrice']
+
 
 # ==================== Update the Airport Codes in Google Sheet ====================
 
@@ -21,3 +24,13 @@ print(f"sheet_data:\n {sheet_data}")
 
 data_manager.destination_data = sheet_data
 data_manager.update_iata_code()
+
+
+# ================================= Get the cheap flight data =================================
+
+# Store the price data for each flight
+flight_data_list = []
+for row in sheet_data:
+    flight_data = FlightData(price=row["lowestPrice"], departure_airport_code=row["iataCode"])
+    flight_data_list.append(flight_data)
+
