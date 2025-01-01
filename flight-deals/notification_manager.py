242406f5c6e60d5870f85fa0c858719a44c23c24
CodeNinjaDan <dnlmuiru@gmail.com>
2025-01-01 23:52:00 +0300

    send notifications & bug fixes

--- flight-deals/notification_manager.py
+++ flight-deals/notification_manager.py
@@ -1,3 +1,23 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv()
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details and using the twilio api.
-    pass
+
+    def __init__(self):
+        self.account_sid = os.getenv("ACCOUNT_SID")
+        self.auth_token = os.getenv("AUTH_TOKEN")
+        self.whatsapp_sender = os.getenv("WHATSAPP_SENDER")
+        self.whatsapp_receiver = os.getenv("WHATSAPP_RECEIVER")
+        self.client = Client(self.account_sid, self.auth_token)
+
+
+    def send_whatsapp(self, message_body):
+        message = self.client.messages.create(
+            from_=self.whatsapp_sender,
+            body=message_body,
+            to=self.whatsapp_receiver
+        )
+        print(message.sid)
